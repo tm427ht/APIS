b77b677ceddb8fd778ea6a256a2c6702f3e76fb0
tm427ht <60478943+tm427ht@users.noreply.github.com>
2021-12-30 19:00:37 +0100

    Update sklad.py

--- sklad.py
+++ sklad.py
@@ -4,34 +4,34 @@ from flask import jsonify
 from flask_cors import CORS
 import mysql.connector as MYSQL
 
-sklad = Flask(__name__)
-CORS(sklad)
+app = Flask(__name__)
+CORS(app)
 
 ovocie = ["jablko", "pomaranc", "jahoda"]
 
-@sklad.route("/", methods = ["GET"])
+@app.route("/", methods = ["GET"])
 def main(): 
     return jsonify({"ovocie": ovocie}), 200
 
-@sklad.route("/vytvorit", methods = ["POST"])
+@app.route("/vytvorit", methods = ["POST"])
 def create(): 
     data = request.get_json(force = True)
     data_dict = dict(data)
     ovocie.append(data_dict["vytvorit"])
     return jsonify("created"), 201
 
-@sklad.route("/upravit/<id>", methods = ["PUT"])
+@app.route("/upravit/<id>", methods = ["PUT"])
 def update(id):
     data = request.get_json(force = True)
     data_dict = dict(data)
     ovocie[int(id)] = data_dict["upravit"]
     return jsonify("update"), 201
 
-@sklad.route("/vymazat/<id>", methods = ["DELETE"])
+@app.route("/vymazat/<id>", methods = ["DELETE"])
 def delete(id): 
     del ovocie[int(id)]
     return jsonify("deleted"), 204
 
 if __name__ == "__main__": 
-    sklad.run
+    app.run
 
