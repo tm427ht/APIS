3b7f66cea208145fe385879fd304ea5c5d01395e
tm427ht <60478943+tm427ht@users.noreply.github.com>
2021-12-30 19:11:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ import mysql.connector as MYSQL
 app = Flask(__name__)
 CORS(app)
 
-ovocie = ["jablko","pomaranc","jahoda"]
+product = {
+    "ID" : {0, 1, 2, 3},
+    "Price" : {150.99, 100.99, 120.99, 200.99},
+    "Name" : {"Volkswagen", "LandRover", "Peugeot", "Skoda"}
+}
 
 @app.route("/", methods=["GET"])
 def main():
@@ -18,19 +22,19 @@ def main():
 def create():
     data = request.get_json(force=True)
     data_dict = dict(data)
-    ovocie.append(data_dict["vytvorit"])
+    product.append(data_dict["vytvorit"])
     return jsonify("created"),201
 
 @app.route("/upravit/<id>", methods=["PUT"])
 def update(id):
     data = request.get_json(force=True)
     data_dict = dict(data)
-    ovocie[int(id)] = data_dict["upravit"]
+    product[int(id)] = data_dict["upravit"]
     return jsonify("updated"),201
 
 @app.route("/vymazat/<id>", methods=["DELETE"])
 def delete(id):
-    del ovocie[int(id)]
+    del product[int(id)]
     return jsonify("deleted"),204
 
 if __name__ == "__main__":
